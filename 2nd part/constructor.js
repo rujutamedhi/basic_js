010e37db2735be25c291f54b6cba66a0b0bd5e18
rujutamedhi <rujutamedhi@gmail.com>
2024-09-16 11:22:27 +0530

    first commit

---
+++ 2nd part/constructor.js
@@ -0,0 +1,17 @@
+
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+  
+    
+    this.printDetails = () => {
+      console.log(`Name: ${this.name}, Age: ${this.age}`);
+    };
+  }
+  
+  const person = new Person('Rujuta', 20);
+  person.printDetails(); 
+
+  
+
+  
---
+++ 2nd part/es6.js
@@ -0,0 +1,11 @@
+class person {
+    constructor(name) {
+      this.name = name;
+    }
+    display = () => { 
+      console.log(`My name is ${this.name}`);
+    }
+  }
+  const p = new person('Rujuta');
+p.display();
+  
